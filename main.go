bc42cd621fae921bbcd2f742f4301d49e1a8c0a2
Justin Artis <jusart1s8@gmail.com>
2020-09-13 16:41:26 -0400

    Added more server specs

--- main.go
+++ main.go
@@ -2,19 +2,35 @@ package main
 
 import(
 "fmt"
+"time"
 "log"
 "net/http"
 "github.com/gorilla/mux"
+"github.com/aws/aws-sdk-go/aws"
+"github.com/aws/aws-sdk-go/aws/session"
+"github.com/aws/aws-sdk-go/service/dynamodb"
 )
 
+const homeMessage = "Hello Wano!"
+
 func home(writer http.ResponseWriter, request *http.Request){
-    fmt.Fprintf(writer, "Hello Wano!")
+    writer.WriteHeader(http.StatusOK)
+    writer.Write([]byte(homeMessage))
 }
 
 
 func main() {
+//     srv := &http.Server{
+//         Addr: ":8080",
+//         ReadTimeout: 5 * time.Second,
+//         WriteTimeout: 10 * time.Second,
+//         IdleTimeout: 120 * time.Second,
+//         Handler: mux,
+//     }
 	router := mux.NewRouter().StrictSlash(true)
     router.HandleFunc("/", home)
     log.Println("Escaping Whole Cake Island!")
-    log.Fatal(http.ListenAndServe(":8080", router))
+    err := http.ListenAndServe(":8080", router)
+    fmt.Println(err)
+    log.Fatalf("Kaido failed the server",err)
 }
